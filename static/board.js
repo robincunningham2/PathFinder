17522515332d822e4d705bdfdc8257de4b1534cf
Robin Cunningham <68775372+robincunningham2@users.noreply.github.com>
2020-12-18 14:57:22 +0100

    Create updateNodes function

--- static/board.js
+++ static/board.js
@@ -1,4 +1,15 @@
 
+function updateNodes(rows) {
+    for (let r = 0; r < rows.length; r++) {
+        const row = rows[r];
+        $('#board').append('<tr></tr>')
+        for (let c = 0; c < row.length; c++) {
+            const col = row[c];
+            $(`#board tr:nth-child(${r + 1})`).append('<td></td>');
+        }
+    }
+}
+
 function getSizes() {
     let h = $(window).height();
     let w = $(window).width();
